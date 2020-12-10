ab330506cb984ea7c589e5714d546f042598391d
nilanjan83 <68443233+nilanjan83@users.noreply.github.com>
2020-12-10 22:51:30 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h1>video calling website</h1>
+      <dl>used for video calling between two people</dl>
+    </div>
+  );
+};
 
 export default App;
